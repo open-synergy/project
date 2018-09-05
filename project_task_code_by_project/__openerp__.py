2c9d96b8723f9602f8aab59e0f4a42e22ca8703d
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-05 09:40:31 +0700

    [8.0.1.0.0][project_task_code_by_project]

---
+++ project_task_code_by_project/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Task Code By Project",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "Opensynergy Indonesia, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project_task_code"
+    ],
+    "data": [
+        "views/project_view.xml"
+    ],
+}
---
+++ project_task_code_by_project/models/project_task.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    @api.model
+    def create(self, vals):
+        _super = super(ProjectTask, self)
+        obj_project = self.env["project.project"]
+        project_id = vals.get("project_id", False)
+
+        if project_id:
+            criteria = [
+                ("id", "=", project_id)
+            ]
+            project = obj_project.search(criteria)
+            if project.task_sequence_id:
+                sequence = self.env["ir.sequence"].\
+                    next_by_id(
+                        project.task_sequence_id.id)
+                vals['code'] = sequence
+        return super(ProjectTask, self).create(vals)
